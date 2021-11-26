96d5955c4e2ba909ab11950889a4970138189eb7
kl3jvi <klejvisiper@gmail.com>
2021-11-26 19:47:53 +0100

    First Commit

---
+++ app/src/main/java/com/kl3jvi/crispytask/ui/main/MainFragment.kt
@@ -0,0 +1,29 @@
+package com.kl3jvi.crispytask.ui.main
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.kl3jvi.crispytask.R
+
+class MainFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private val viewModel: MainViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.main_fragment, container, false)
+    }
+
+
+
+}
---
+++ app/src/main/java/com/kl3jvi/crispytask/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.kl3jvi.crispytask.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
