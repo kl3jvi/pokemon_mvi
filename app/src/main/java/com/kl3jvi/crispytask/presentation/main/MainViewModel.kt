d52f2efe0030ae729897eae8261765d9f61f9ff6
kl3jvi <klejvisiper@gmail.com>
2021-11-26 22:31:48 +0100

    Fixing Moshi Kotlin Version

--- app/src/main/java/com/kl3jvi/crispytask/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/kl3jvi/crispytask/presentation/main/MainViewModel.kt
@@ -2,8 +2,9 @@ package com.kl3jvi.crispytask.presentation.main
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
     // TODO: Implement the ViewModel
 }
