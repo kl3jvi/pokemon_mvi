e07bdf8f0a3f6bb75d90c16f82b0ddee9bbf098d
kl3jvi <klejvisiper@gmail.com>
2021-11-26 23:08:41 +0100

    Implementing Uniflow-kt

--- app/src/main/java/com/kl3jvi/crispytask/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/kl3jvi/crispytask/presentation/main/MainViewModel.kt
@@ -6,5 +6,5 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor() : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
---
+++ app/src/main/java/com/kl3jvi/crispytask/presentation/main/state/MainState.kt
@@ -0,0 +1,12 @@
+package com.kl3jvi.crispytask.presentation.main.state
+
+import io.uniflow.core.flow.data.UIState
+
+/**
+ * UIState() as said on example readme I am using uniflow-kt for state handling,
+ * usually I use custom Sealed Classes
+ */
+
+open class MainState : UIState() {
+
+}
